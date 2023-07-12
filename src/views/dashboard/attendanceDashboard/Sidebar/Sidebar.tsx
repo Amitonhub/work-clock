70e69c2fb625087e7ffe8fffd87d5fa7598932d0
karun gupta <ikarungupta@gmail.com>
2023-07-12 11:56:33 +0530

    notification bar

--- src/views/dashboard/attendanceDashboard/Sidebar/Sidebar.tsx
+++ src/views/dashboard/attendanceDashboard/Sidebar/Sidebar.tsx
@@ -1,5 +1,13 @@
-import React from "react";
-import { Drawer, List, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Tab,
+  Tabs,
+} from "@mui/material";
 import { Notifications, Close } from "@mui/icons-material";
 import styles from "./Sidebar.module.css";
 
@@ -9,6 +17,25 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
+  const [activeTab, setActiveTab] = useState(0);
+  const [notifications, setNotifications] = useState([
+    "Notification 1",
+    "Notification 2",
+    "Notification 3"
+  ]);
+
+  const handleChangeTab = (event: React.SyntheticEvent, newTab: number) => {
+    setActiveTab(newTab);
+  };
+
+  const handleRemoveNotification = (index: number) => {
+    setNotifications((prevNotifications) => {
+      const updatedNotifications = [...prevNotifications];
+      updatedNotifications.splice(index, 1);
+      return updatedNotifications;
+    });
+  };
+
   return (
     <Drawer
       anchor="right"
@@ -21,17 +48,66 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
       variant="persistent"
     >
       <div className={styles.sidebarHeader}>
-        <div className={styles.arrowIcon} onClick={onClose}>
-          <Close className={styles.arrowIconImage} />
+        <div className={styles.headerDiv}>
+          <h4>Notifications</h4>
+        </div>
+        <div className={styles.closeIcon} onClick={onClose}>
+          <Close className={styles.closeIconImage} />
         </div>
       </div>
+
+      <Tabs
+        value={activeTab}
+        onChange={handleChangeTab}
+        variant="fullWidth"
+        textColor="inherit"
+        indicatorColor="secondary"
+      >
+        <Tab label="Recent" />
+        <Tab label="HR Approved" />
+      </Tabs>
       <List>
-        <ListItemButton>
-          <ListItemIcon>
-            <Notifications />
-          </ListItemIcon>
-          <ListItemText primary="Notifications"/>
-        </ListItemButton>
+        {activeTab === 0 &&
+          notifications.map((notification, index) => (
+            <ListItem
+              key={index}
+              className={styles.notificationItem}
+              onClick={() => handleRemoveNotification(index)}
+            >
+              <ListItemIcon>
+                <Notifications className={styles.NotificationsIcon} />
+              </ListItemIcon>
+              <ListItemText
+                primary={notification}
+                className={styles.NotificationsText}
+              />
+              <div className={styles.closeButton}>
+                <Close />
+              </div>
+            </ListItem>
+          ))}
+        {activeTab === 1 && (
+          <>
+            <ListItem className={styles.notificationItem}>
+              <ListItemIcon>
+                <Notifications className={styles.NotificationsIcon} />
+              </ListItemIcon>
+              <ListItemText
+                primary="HR Approved 1"
+                className={styles.NotificationsText}
+              />
+            </ListItem>
+            <ListItem className={styles.notificationItem}>
+              <ListItemIcon>
+                <Notifications className={styles.NotificationsIcon} />
+              </ListItemIcon>
+              <ListItemText
+                primary="HR Approved 2"
+                className={styles.NotificationsText}
+              />
+            </ListItem>
+          </>
+        )}
       </List>
     </Drawer>
   );
