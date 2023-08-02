35ba727f70a26df0110522ddac34d71e006dcacc
Amit Thakur <amitthakurat4615321@gmail.com>
2023-08-02 15:10:48 +0530

    Qr Scanner (working) first Commit

--- src/views/dashboard/attendanceDashboard/QRCodeScanner/index.tsx
+++ src/views/dashboard/attendanceDashboard/QRCodeScanner/index.tsx
@@ -1,38 +1,40 @@
-import React, { useState, useRef } from "react";
-import QrScanner from 'react-qr-scanner';
+'use client'
+import React, { useEffect, useState } from "react";
+import { Html5QrcodeScanner } from "html5-qrcode";
+import Link from "next/link";
 
-interface QRCodeScannerProps {
-  onScan: (data: string | null) => void;
-  delay?: number; 
+function QrScanner() {
+    const [scanResult, setScanResult] = useState()
+
+    useEffect(() => {
+        const scanner = new Html5QrcodeScanner('reader', {
+            qrbox: {
+                width: 250,
+                height: 250,
+            },
+            fps: 5,
+            aspectRatio: 1.00,
+            showTorchButtonIfSupported: true
+        }, false)
+        scanner.render(success, error)
+    
+        function success(result: any) {
+            scanner.clear()
+            setScanResult(result)
+        }
+    
+        function error(err: any) {
+            console.warn(err)
+        }
+    }, [])
+
+    return <>
+    <h1>Qr Scanner</h1>
+    {scanResult
+    ? <div>Success: <Link href={`/${scanResult}`}>{scanResult}</Link></div>
+    : <div id="reader"></div>
+    }
+    </>;
 }
 
-const QRCodeScanner: React.FC<QRCodeScannerProps> = ({ onScan, delay = 300 }) => {
-  const [scannedData, setScannedData] = useState<string | null>(null);
-  const scannerRef = useRef<QrScanner | null>(null);
-
-  const handleScan = (data: string | null) => {
-    if (data) {
-      setScannedData(data);
-      onScan(data);
-    }
-  };
-
-  const handleError = (error: Error) => {
-    console.error(error);
-  };
-
-  return (
-    <div>
-      <QrScanner
-        ref={(scanner: QrScanner | null) => (scannerRef.current = scanner)}
-        delay={delay}
-        onError={handleError}
-        onScan={handleScan}
-        style={{ width: "100%" }}
-      />
-      {scannedData && <p>Scanned Data: {scannedData}</p>}
-    </div>
-  );
-};
-
-export default QRCodeScanner;
+export default QrScanner;
