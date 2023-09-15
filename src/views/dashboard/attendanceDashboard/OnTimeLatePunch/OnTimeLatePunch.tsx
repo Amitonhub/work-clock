7bd6cf5a56ad0e0193584de9f2435272a1bd631e
karun <ikarungupta@gmail.com>
2023-09-15 15:20:50 +0530

    data on dashboard for timeline & ontimelatepunch

--- src/views/dashboard/attendanceDashboard/OnTimeLatePunch/OnTimeLatePunch.tsx
+++ src/views/dashboard/attendanceDashboard/OnTimeLatePunch/OnTimeLatePunch.tsx
@@ -1,18 +1,90 @@
 import { Box, CircularProgress, Skeleton } from "@mui/material";
 import styles from "./OnTimeLatePunch.module.css";
 import { useEffect, useState } from "react";
+import moment from "moment";
+import { useGetAllAttendanceByIdQuery } from "@/redux/services/attendanceApi";
+import { useAppSelector } from "@/redux/store";
+import {
+  AttendanceDataType,
+  Punch,
+} from "@/views/dashboard/types/attendanceDataType";
+import { AttendanceTypes } from "../../types/attendanceType";
 
 const OnTimeLatePunch = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [onTimeCount, setOnTimeCount] = useState(0);
+  const [lateCount, setLateCount] = useState(0);
+
+  const user = useAppSelector((state) => state.user.UserData);
+  const {
+    data,
+    isSuccess,
+    isLoading: isFetching,
+  } = useGetAllAttendanceByIdQuery(user?.id);
+  const formattedData = data?.map((item: AttendanceDataType) => ({
+    ...item,
+    date: new Date(item.date),
+  }));
 
   useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    });
-    return () => {
-      setIsLoading(false);
-    };
-  }, []);
+    if (isSuccess) {
+      const currentDate = moment();
+
+      if (currentDate.date() === 1) {
+        // for 1 month punch-in only
+        setOnTimeCount(0);
+        setLateCount(0);
+      }
+
+      if (currentDate.date() <= moment().daysInMonth()) {
+        const onTimeStartTime = moment().set({
+          hour: 9,
+          minute: 30,
+          second: 0,
+        });
+        const onTimeEndTime = moment().set({
+          hour: 10,
+          minute: 15,
+          second: 0,
+        });
+        const lateEndTime = moment().set({
+          hour: 19,
+          minute: 0,
+          second: 0,
+        });
+
+        let onTimeCount = 0;
+        let lateCount = 0;
+
+        formattedData.forEach((item: AttendanceDataType) => {
+          item.punches.forEach((punch: Punch) => {
+            const punchTime = moment(punch.timestamp);
+            const formattedPunchTime = punchTime.format("h:mm:ss");
+            if (punch.type === AttendanceTypes.punchIn) {
+              if (
+                formattedPunchTime > onTimeStartTime.format("h:mm:ss") &&
+                formattedPunchTime < onTimeEndTime.format("h:mm:ss")
+              ) {
+                onTimeCount++;
+              } else if (
+                formattedPunchTime > onTimeEndTime.format("h:mm:ss") &&
+                formattedPunchTime < lateEndTime.format("h:mm:ss")
+              ) {
+                lateCount++;
+              } else {
+                onTimeCount++;
+              }
+            }
+          });
+        });
+
+        setOnTimeCount(onTimeCount);
+        setLateCount(lateCount);
+      }
+    }
+    setIsLoading(false);
+  }, [isSuccess, formattedData]);
+  const totalPunches = onTimeCount + lateCount;
 
   return (
     <div className={styles.ontimeLatePunchDiv}>
@@ -41,22 +113,39 @@ const OnTimeLatePunch = () => {
             }}
           >
             <div className={styles.contentMainDiv}>
-              <CircularProgress
-                variant="determinate"
-                value={80}
-                size={50}
-                thickness={4}
-                color="success"
-              />
+              {isFetching ? (
+                <Skeleton
+                  variant="circular"
+                  animation="wave"
+                  width={50}
+                  height={50}
+                />
+              ) : (
+                <CircularProgress
+                  variant="determinate"
+                  value={(onTimeCount / totalPunches) * 100}
+                  size={50}
+                  thickness={4}
+                  color="success"
+                />
+              )}
               <div
                 className={styles.circularDiv}
                 style={{ color: "green", transform: "translate(-50px)" }}
               >
-                80%
+                {isFetching ? (
+                  <Skeleton variant="text" animation="wave" width={30} />
+                ) : (
+                  `${Math.round((onTimeCount / totalPunches) * 100)}%`
+                )}
               </div>
               <div>
                 <h5 className={styles.headingDiv}>On Time</h5>
-                <h6 className={styles.suHeadingOnTime}>8</h6>
+                {isFetching ? (
+                  <Skeleton variant="text" animation="wave" width={40} />
+                ) : (
+                  <h6 className={styles.suHeadingOnTime}>{onTimeCount}</h6>
+                )}
               </div>
             </div>
           </Box>
@@ -87,22 +176,39 @@ const OnTimeLatePunch = () => {
             }}
           >
             <div className={styles.contentMainDiv}>
-              <CircularProgress
-                variant="determinate"
-                value={20}
-                size={50}
-                thickness={4}
-                color="warning"
-              />
+              {isFetching ? (
+                <Skeleton
+                  variant="circular"
+                  animation="wave"
+                  width={50}
+                  height={50}
+                />
+              ) : (
+                <CircularProgress
+                  variant="determinate"
+                  value={(lateCount / totalPunches) * 100}
+                  size={50}
+                  thickness={4}
+                  color="warning"
+                />
+              )}
               <div
                 className={styles.circularDiv}
                 style={{ color: "goldenrod", transform: "translate(-38px)" }}
               >
-                20%
+                {isFetching ? (
+                  <Skeleton variant="text" animation="wave" width={30} />
+                ) : (
+                  `${Math.round((lateCount / totalPunches) * 100)}%`
+                )}
               </div>
               <div>
                 <h5 className={styles.headingDiv}>Late</h5>
-                <h6 className={styles.suHeadinglate}>2</h6>
+                {isFetching ? (
+                  <Skeleton variant="text" animation="wave" width={40} />
+                ) : (
+                  <h6 className={styles.suHeadinglate}>{lateCount}</h6>
+                )}
               </div>
             </div>
           </Box>
