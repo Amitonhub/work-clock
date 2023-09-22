66c5f97a084260c70968a8255ae4fa175e33948e
Amit Thakur <amitthakurat4615321@gmail.com>
2023-09-22 16:59:40 +0530

    remove gitignore files from backend

--- backend/helpers/corsOptions.js
+++ backend/helpers/corsOptions.js
@@ -1,8 +1,8 @@
-const corsOptions ={
-    // origin:process.env.VERCEL_URL,          // uncomment for Live URL
-    origin:process.env.HOST_URL,            // uncomment for Local URL
-    credentials:true,           
-    optionSuccessStatus:200
+const corsOptions = {
+    origin: process.env.VERCEL_URL, // uncomment for Live URL
+    // origin:process.env.HOST_URL,            // uncomment for Local URL
+    credentials: true,
+    optionSuccessStatus: 200
 }
 
 module.exports = corsOptions
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL          // uncomment for Local Api
-// export const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_API_URL      // uncomment for Live Api
+// export const BASE_URL = process.env.NEXT_PUBLIC_API_URL          // uncomment for Local Api
+export const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_API_URL      // uncomment for Live Api
 export const SESSION_EXPIRED = "Session Expired!"
 export enum variables {
     COUNTDOWN_CLOCK_DURATION = 28800, // For 8 Hours Of Work
